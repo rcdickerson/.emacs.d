8d8e5db960c4f2db2880c92e73773e96293536ff
Rob Dickerson <rdickerson@squareup.com>
2015-09-15 20:33:19 -0500

    Fixing up hideshow for protobuf mode.

--- settings/setup-hideshow-mode.el
+++ settings/setup-hideshow-mode.el
@@ -1,5 +1,10 @@
 (add-hook 'prog-mode-hook #'hs-minor-mode)
 
+(add-to-list 'hs-special-modes-alist
+	     '(protobuf-mode
+	       "\\(/\\*\\|{\\)" "\\(\\*/\\|}\\)" "//"
+         nil))
+
 (global-set-key (kbd "C-c C-,") 'hs-hide-block)
 (global-set-key (kbd "C-c C-.") 'hs-show-block)
 
