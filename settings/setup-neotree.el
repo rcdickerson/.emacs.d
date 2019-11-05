8fa7e7a0e527331aff2987139d3a43ba5ff2c409
Rob Dickerson <rc.dickerson@gmail.com>
2019-11-05 13:40:52 -0500

    Adding some niceties for neotree window size.

--- settings/setup-neotree.el
+++ settings/setup-neotree.el
@@ -4,4 +4,16 @@
 ;; Hook into projectile.
 (setq projectile-switch-project-action 'neotree-projectile-action)
 
+;; Don't force neotree window into a fixed size
+(setq neo-window-fixed-size nil)
+
+;; Remember neotree window size between close / open
+;; https://github.com/jaypei/emacs-neotree/issues/262
+ (eval-after-load "neotree"
+   '(add-to-list 'window-size-change-functions
+                 (lambda (frame)
+                   (let ((neo-window (neo-global--get-window)))
+                     (unless (null neo-window)
+                       (setq neo-window-width (window-width neo-window)))))))
+
 (provide 'setup-neotree)
