383e1d46ff6dc3bbcc0fb7f284736b159bda318a
Robert Dickerson <rob@robd.io>
2022-06-03 16:10:39 -0500

    Updating MELPA URL.

--- settings/setup-package.el
+++ settings/setup-package.el
@@ -1,7 +1,7 @@
 (require 'package)
 (require 'cl)
 
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 
 (package-initialize)
 
