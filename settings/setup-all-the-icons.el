6312ddfb63590536ef454493f7601ed6d6dea8d0
Robert Dickerson <rob@robd.io>
2023-11-25 01:31:09 -0500

    Rustic settings.

---
+++ settings/setup-all-the-icons.el
@@ -0,0 +1,4 @@
+(use-package all-the-icons
+  :if (display-graphic-p))
+
+(provide 'setup-all-the-icons)
--- settings/setup-rustic.el
+++ settings/setup-rustic.el
@@ -1,3 +1,8 @@
-(use-package rustic)
+(use-package rustic
+  :bind (:map rustic-mode-map
+              ("M-j" . lsp-ui-imenu)
+              ("C-c C-c l" . flycheck-list-errors)
+              ("C-c C-c a" . lsp-execute-code-action)
+              ("C-c C-c r" . lsp-rename)))
 
 (provide 'setup-rustic)
